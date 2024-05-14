9a732500ad647e0c28a2aad3149276fc2809fcf1
ainurka0407 <a.nurzhanova111@gmail.com>
2024-05-14 13:12:23 +0500

    added binary search tree

---
+++ src/BinarySearchTree/BST.java
@@ -0,0 +1,123 @@
+package BinarySearchTree;
+
+import java.util.ArrayList;
+import java.util.Iterator;
+import java.util.List;
+import java.util.Stack;
+
+public class BST<K extends Comparable<K>, V> implements Iterable<BST<K, V>.Node> {
+    private Node root;
+
+    public class Node {
+        private K key;
+        private V val;
+        private Node left, right;
+
+        public Node(K key, V val) {
+            this.key = key;
+            this.val = val;
+        }
+
+        public K getKey() {
+            return key;
+        }
+
+        public V getValue() {
+            return val;
+        }
+    }
+
+    // Insert a key-value pair into the BST
+    public void put(K key, V val) {
+        root = put(root, key, val);
+    }
+
+    private Node put(Node x, K key, V val) {
+        if (x == null) return new Node(key, val);
+        int cmp = key.compareTo(x.key);
+        if (cmp < 0) x.left = put(x.left, key, val);
+        else if (cmp > 0) x.right = put(x.right, key, val);
+        else x.val = val;
+        return x;
+    }
+
+    // Retrieve the value associated with the given key
+    public V get(K key) {
+        Node x = get(root, key);
+        if (x == null) return null;
+        return x.val;
+    }
+
+    private Node get(Node x, K key) {
+        if (x == null) return null;
+        int cmp = key.compareTo(x.key);
+        if (cmp < 0) return get(x.left, key);
+        else if (cmp > 0) return get(x.right, key);
+        else return x;
+    }
+
+    // Delete a key-value pair from the BST
+    public void delete(K key) {
+        root = delete(root, key);
+    }
+
+    private Node delete(Node x, K key) {
+        if (x == null) return null;
+        int cmp = key.compareTo(x.key);
+        if (cmp < 0) x.left = delete(x.left, key);
+        else if (cmp > 0) x.right = delete(x.right, key);
+        else {
+            if (x.right == null) return x.left;
+            if (x.left == null) return x.right;
+            Node t = x;
+            x = min(t.right);
+            x.right = deleteMin(t.right);
+            x.left = t.left;
+        }
+        return x;
+    }
+
+    private Node min(Node x) {
+        if (x.left == null) return x;
+        else return min(x.left);
+    }
+
+    private Node deleteMin(Node x) {
+        if (x.left == null) return x.right;
+        x.left = deleteMin(x.left);
+        return x;
+    }
+
+    // Iterator for in-order traversal
+    @Override
+    public Iterator<Node> iterator() {
+        return new BSTIterator(root);
+    }
+
+    private class BSTIterator implements Iterator<Node> {
+        private Stack<Node> stack = new Stack<>();
+
+        public BSTIterator(Node root) {
+            pushLeft(root);
+        }
+
+        private void pushLeft(Node x) {
+            while (x != null) {
+                stack.push(x);
+                x = x.left;
+            }
+        }
+
+        @Override
+        public boolean hasNext() {
+            return !stack.isEmpty();
+        }
+
+        @Override
+        public Node next() {
+            Node node = stack.pop();
+            pushLeft(node.right);
+            return node;
+        }
+    }
+}
---
+++ src/BinarySearchTree/Main.java
@@ -0,0 +1,52 @@
+package BinarySearchTree;
+
+import java.util.Iterator;
+
+public class Main {
+    public static void main(String[] args) {
+        BST<Integer, String> bst = new BST<>();
+
+        bst.put(50, "Value50");
+        bst.put(30, "Value30");
+        bst.put(20, "Value20");
+        bst.put(40, "Value40");
+        bst.put(70, "Value70");
+        bst.put(60, "Value60");
+        bst.put(80, "Value80");
+
+        System.out.println("Retrieving key 50: " + bst.get(50)); // Output: Value50
+        System.out.println("Retrieving key 20: " + bst.get(20)); // Output: Value20
+        System.out.println("Retrieving key 70: " + bst.get(70)); // Output: Value70
+
+        System.out.println("In-order traversal:");
+        Iterator<BST<Integer, String>.Node> iterator = bst.iterator();
+        while (iterator.hasNext()) {
+            BST<Integer, String>.Node node = iterator.next();
+            System.out.println("Key is " + node.getKey() + " and value is " + node.getValue());
+        }
+
+        bst.delete(20);
+        System.out.println("In-order traversal after deleting key 20:");
+        iterator = bst.iterator();
+        while (iterator.hasNext()) {
+            BST<Integer, String>.Node node = iterator.next();
+            System.out.println("Key is " + node.getKey() + " and value is " + node.getValue());
+        }
+
+        bst.delete(30);
+        System.out.println("In-order traversal after deleting key 30:");
+        iterator = bst.iterator();
+        while (iterator.hasNext()) {
+            BST<Integer, String>.Node node = iterator.next();
+            System.out.println("Key is " + node.getKey() + " and value is " + node.getValue());
+        }
+
+        bst.delete(50);
+        System.out.println("In-order traversal after deleting key 50:");
+        iterator = bst.iterator();
+        while (iterator.hasNext()) {
+            BST<Integer, String>.Node node = iterator.next();
+            System.out.println("Key is " + node.getKey() + " and value is " + node.getValue());
+        }
+    }
+}
